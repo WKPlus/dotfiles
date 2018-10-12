38b1d73bf6936cd2d4b668e76d38e495bc71cea8
wkplus <wkplus@wkplusdeMacBook-Pro.local>
2018-10-12 12:06:05 +0800

    add a tutorial for install vim

---
+++ tools/install_vim.sh
@@ -0,0 +1,7 @@
+
+mkdir -p ~/.vim/bundle ~/.vim/colors
+cd ~/.vim/bundle && git clone https://github.com/Shougo/neobundle.vim.git
+# open vim, run NeoBundleInstall
+
+# copy bundle/vim-molokai256/colors/molokai256.vim colors/
+
